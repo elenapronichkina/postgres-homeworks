c005ccbb9b395ca2ff041dc19c102eec2b5294aa
Elena Pronichkina <prolena39@mail.ru>
2023-08-10 20:10:52 +0300

    add code groupby.sql

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -20,9 +20,22 @@ GROUP BY country
 ORDER BY COUNT(supplier_id) DESC;
 
 -- 5. суммарный вес заказов (в которых известен регион) по странам, но вывести только те результаты, где суммарный вес на страну больше 2750. Отсортировать по убыванию суммарного веса (см таблицу orders, колонки ship_region, ship_country, freight)
-
+SELECT ship_country, SUM (freight)
+FROM orders
+GROUP BY ship_country
+HAVING SUM (freight) > 2750
+ORDER BY SUM (freight) DESC;
 
 -- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers) и работники (employees).
-
+SELECT country FROM customers
+INTERSECT
+SELECT country FROM suppliers
+INTERSECT
+SELECT country FROM employees;
 
 -- 7. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers), но не зарегистрированы работники (employees).
+SELECT country FROM customers
+INTERSECT
+SELECT country FROM suppliers
+EXCEPT
+SELECT country FROM employees;
