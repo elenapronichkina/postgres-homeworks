abe675cdef1cc82a2880ac3796e154cb1ba9fbfd
Elena Pronichkina <prolena39@mail.ru>
2023-08-14 22:38:10 +0300

    correct code

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,11 +1,12 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT company_name, CONCAT(employees.first_name,' ',employees.last_name) AS employee
-FROM customers
-INNER JOIN employees USING(city)
-INNER JOIN shippers USING(company_name)
-WHERE city = 'London'
+SELECT customers.company_name, CONCAT(employees.first_name,' ',employees.last_name) AS employee
+FROM orders
+INNER JOIN customers USING(customer_id)
+INNER JOIN employees USING(employee_id)
+INNER JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city = 'London' AND employees.city = 'London'
 AND shippers.company_name = 'United Package';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
@@ -21,12 +22,11 @@ AND categories.category_name IN ('Dairy Products','Condiments')
 ORDER BY units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
+--Получаем данные из таблицы customers соединяем ее join с таблицей orders и в фильтровке проверяем что order_id null
+
 SELECT company_name FROM customers
-INNER JOIN orders USING(customer_id)
-WHERE EXISTS
-(SELECT customer_id FROM customers
-EXCEPT
-SELECT customer_id FROM orders)
+FULL JOIN orders USING(customer_id)
+WHERE order_id IS NULL;
 
 --- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
